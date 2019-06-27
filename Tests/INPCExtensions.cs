b1cf340a147308e3a8bec6203413980f0fccb631
Alex Shelley <awshelley@beckman.com>
2019-06-27 17:18:21 -0400

    Add AssertChangesProperty extension method.

---
+++ Tests/INPCExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xunit;
+
+namespace Tests
+{
+    public static class INPCExtensions
+    {
+        public static void AssertChangesProperty(this INotifyPropertyChanged obj, string property, Action action)
+        {
+            bool changed = false;
+            void DetectChanges(object sender, PropertyChangedEventArgs e)
+            {
+                if (e.PropertyName == property)
+                    changed = true;
+            }
+
+            obj.PropertyChanged += DetectChanges;
+            action();
+            obj.PropertyChanged -= DetectChanges;
+
+            Assert.True(changed);
+        }
+    }
+}
