de880cec7758272329a74b5ccccf50a6766a7797
Alex Shelley <awshelley@beckman.com>
2019-06-27 17:14:36 -0400

    Add Point and NotifyChangedAttribute.

---
+++ AssemblyToProcess/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+using AutoPropertyChanged;
+
+namespace AssemblyToProcess
+{
+    public class Point : INotifyPropertyChanged
+    {
+        [NotifyChanged] public int X { get; set; }
+        [NotifyChanged] public int Y { get; set; }
+        [NotifyChanged] public int Z { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
