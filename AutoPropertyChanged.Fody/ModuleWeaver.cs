0a3b1ffcfa42697016a3ff09396f1ac8abf5f891
Alex Shelley <awshelley@beckman.com>
2019-06-27 16:43:52 -0400

    Copy starter project from BasicFodyAddin.  Delete unnecessary stuff and rebrand it as AutoPropertyChanged.

---
+++ AutoPropertyChanged.Fody/ModuleWeaver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Mono.Cecil;
+using Mono.Cecil.Rocks;
+using Mono.Cecil.Cil;
+using Fody;
+
+public class ModuleWeaver : BaseModuleWeaver
+{
+    public override bool ShouldCleanReference => true;
+
+    public override void Execute()
+    {
+    }
+
+    public override IEnumerable<string> GetAssembliesForScanning()
+    {
+        yield return "netstandard";
+        yield return "mscorlib";
+    }
+}
---
+++ Tests/WeaverTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Fody;
+using Xunit;
+
+#region WeaverTests
+
+public class WeaverTests
+{
+    static TestResult testResult;
+
+    static WeaverTests()
+    {
+        var weavingTask = new ModuleWeaver();
+        testResult = weavingTask.ExecuteTestRun("AssemblyToProcess.dll");
+    }
+}
+
+#endregion
