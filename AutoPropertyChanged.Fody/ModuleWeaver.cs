535e55ba2eb574eec0c878e0cf6cf3c888ba61a0
ashelley <ashelley@purdue.edu>
2019-06-30 17:31:05 -0400

    It now adds InvokePropertyChanged to all classes that get weaved.

--- AutoPropertyChanged.Fody/ModuleWeaver.cs
+++ AutoPropertyChanged.Fody/ModuleWeaver.cs
@@ -5,6 +5,8 @@ using Mono.Cecil;
 using Mono.Cecil.Rocks;
 using Mono.Cecil.Cil;
 using Fody;
+using System.ComponentModel;
+using System.Reflection;
 
 public class ModuleWeaver : BaseModuleWeaver
 {
@@ -22,8 +24,13 @@ public class ModuleWeaver : BaseModuleWeaver
                 .Properties
                 .Where(p => ShouldBeWeaved(p));
 
+            if (!taggedProperties.Any())
+                continue;
+
+            inpcClass.Methods.Add(CreateInvokePropertyChanged(inpcClass));
+
             foreach (var property in taggedProperties)
-                throw new Exception($"{property} should be weaved.");
+                WeaveProperty(property);
         }
     }
 
@@ -36,6 +43,54 @@ public class ModuleWeaver : BaseModuleWeaver
         .CustomAttributes
         .Where(c => c.AttributeType.Name == "NotifyChangedAttribute")
         .Any();
+
+    private void WeaveProperty(PropertyDefinition p)
+    {
+
+    }
+
+    private MethodDefinition CreateInvokePropertyChanged(TypeDefinition type)
+    {
+        ConstructorInfo argsConstructor = typeof(PropertyChangedEventArgs)
+            .GetConstructor(new[] { typeof(string) });
+
+        FieldReference propertyChanged = type
+            .Fields
+            .Where(f => f.Name == "PropertyChanged")
+            .Single();
+
+        MethodInfo invoke = typeof(PropertyChangedEventHandler)
+            .GetMethod("Invoke");
+
+        var method = new MethodDefinition("InvokePropertyChanged", Mono.Cecil.MethodAttributes.Private, TypeRef(typeof(void)));
+        method.Parameters.Add(new ParameterDefinition("propertyName", Mono.Cecil.ParameterAttributes.None, TypeRef<string>()));
+        var processor = method.Body.GetILProcessor();
+
+        // Equivalent to:
+        //      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        // Bail out if it has no subscribers.
+        Instruction ret = processor.Create(OpCodes.Ret);
+
+        processor.Emit(OpCodes.Ldarg_0);
+        processor.Emit(OpCodes.Ldfld, propertyChanged);
+        processor.Emit(OpCodes.Ldnull);
+        processor.Emit(OpCodes.Beq, ret);
+
+        // Call the Invoke method.
+        processor.Emit(OpCodes.Ldarg_0);
+        processor.Emit(OpCodes.Ldfld, propertyChanged);
+        processor.Emit(OpCodes.Ldarg_0);
+        processor.Emit(OpCodes.Ldarg_1);
+        processor.Emit(OpCodes.Newobj, ModuleDefinition.ImportReference(argsConstructor));
+        processor.Emit(OpCodes.Callvirt, ModuleDefinition.ImportReference(invoke));
+
+        processor.Append(ret);
+
+        return method;
+    }
+
+    private TypeReference TypeRef(Type t) => ModuleDefinition.ImportReference(t);
+    private TypeReference TypeRef<T>() => ModuleDefinition.ImportReference(typeof(T));
 
     public override IEnumerable<string> GetAssembliesForScanning()
     {
--- Tests/WeaverTests.cs
+++ Tests/WeaverTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Reflection;
 using Fody;
 using Xunit;
 
@@ -7,25 +8,47 @@ namespace Tests
 {
     public class WeaverTests
     {
-        static TestResult testResult;
-
+        private static TestResult testResult;
+        private static Type pointType;
         static WeaverTests()
         {
             var weavingTask = new ModuleWeaver();
             testResult = weavingTask.ExecuteTestRun("AssemblyToProcess.dll");
+            pointType = testResult.Assembly.GetType("AssemblyToProcess.Point");
+        }
+
+        [Fact]
+        public void InvokePropertyChanged_Gets_Added()
+        {
+            var point = Activator.CreateInstance(pointType);
+            ((INotifyPropertyChanged)point).AssertChangesProperty("X", () =>
+            {
+                InvokePrivateMethod(point, "InvokePropertyChanged", "X");
+            });
+        }
+
+        [Fact]
+        public void InvokePropertyChanged_Doesnt_Crash_If_No_Subscribers()
+        {
+            var point = Activator.CreateInstance(pointType);
+            InvokePrivateMethod(point, "InvokePropertyChanged", "X");
         }
         
         [Fact]
         public void Changing_Property_Fires_Event()
         {
-            var type = testResult.Assembly.GetType("AssemblyToProcess.Point");
-            var point = (dynamic)Activator.CreateInstance(type);
+            var point = (dynamic)Activator.CreateInstance(pointType);
             point.X = 0;
             point.Y = 1;
             point.Z = 2;
 
             ((INotifyPropertyChanged)point).AssertChangesProperty("X", () => point.X = 1);
         }
+
+        private void InvokePrivateMethod(object obj, string name, params object[] args) => obj
+            .GetType()
+            .GetMethod(name, BindingFlags.NonPublic | BindingFlags.Instance)
+            .Invoke(obj, args);
     }
 
 }
