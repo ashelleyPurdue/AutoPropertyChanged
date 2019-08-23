60ea4df74bfc61343917360a4dcdd48e8eef96bf
Alex Shelley <awshelley@beckman.com>
2019-08-23 14:12:12 -0400

    add DependsOnAttribute, but it doesn't do anything yet.

---
+++ AutoPropertyChanged/Attributes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AutoPropertyChanged
+{
+    public class NotifyChangedAttribute : Attribute { }
+
+    public class DependsOnAttribute : Attribute
+    {
+        public string Property { get; set; }
+    }
+}
